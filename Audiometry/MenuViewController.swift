8a975403e51b11ffa00a8c4c76472c102d218f66
candyhero <wchan008@uw.edu>
2017-08-02 09:53:54 -0700

    Testing out on UI
    - Added two more storyboards: Menu & Main UI
        - User can now select to open the main program or calibrate from
    the menu
    - Added SKScene to Main UI
        - Will be used as the animation scene for any visual effects for
    reinforcement purpose, or animation for UI elements such as buttons

---
+++ Audiometry/MenuViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MenuViewController.swift
+//  Audiometry
+//
+//  Created by Xavier Chan on 7/27/17.
+//  Copyright © 2017 Xavier Chan. All rights reserved.
+//
+
+import UIKit
+import AudioKit
+import MediaPlayer
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func colorChange(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.gray
+    }
+    
+    @IBAction func colorRevert(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.blue
+    }
+}
